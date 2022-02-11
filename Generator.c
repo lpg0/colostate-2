a061a1bdbc4c11fe1ab84815aa2a5f427141e0e4
Luke Giraudeau <lpg9de@richmond.cs.colostate.edu>
2022-02-10 17:04:56 -0700

    all

--- Generator.c
+++ Generator.c
@@ -12,7 +12,7 @@ int main( int argc, char *argv[] )  {
       FILE *fp = fopen(fn, "r");
       pid_t pid, pid_main;
       unsigned char buf[256];
-      int i, status;
+      int i, status, ret1, ret2, ret3;
       char *f_lucas = "lucas";
       char *f_hexagonal = "hexagonal";
       char *f_harmonic = "harmonic";
@@ -43,6 +43,7 @@ int main( int argc, char *argv[] )  {
             wait(&status);
             if (WIFEXITED(status)) {
                printf("[Generator] [%d]: The child process %d returned %d\n", pid_main, pid, WEXITSTATUS(status));
+               ret1 = WEXITSTATUS(status);
                sprintf(f_buf, "%d", WEXITSTATUS(status));
             }
          }
@@ -57,6 +58,7 @@ int main( int argc, char *argv[] )  {
             wait(&status);
             if (WIFEXITED(status)) {
                printf("[Generator] [%d]: The child process %d returned %d\n", pid_main, pid, WEXITSTATUS(status));         
+               ret2 = WEXITSTATUS(status);
                sprintf(f_buf, "%d", WEXITSTATUS(status));
             }
          }
@@ -64,17 +66,21 @@ int main( int argc, char *argv[] )  {
          pid = fork();
          if (pid == 0) {
             /* child: exec HarmonicSeries.c */
-            execlp(f_harmonic, f_harmonic, f_buf, NULL);
+            ret3 = execlp(f_harmonic, f_harmonic, f_buf, NULL); 
          } else {
             /* parent: wait for HarmonicSeries.c */
             printf("[Generator] [%d]: Waiting for the child process %d\n", pid_main, pid);
             wait(&status);
             if (WIFEXITED(status)) {
                printf("[Generator] [%d]: The child process %d returned %d\n", pid_main, pid, WEXITSTATUS(status));
+               ret3 = WEXITSTATUS(status);
                sprintf(f_buf, "%d", WEXITSTATUS(status));
             }
          }
          /* loop for every line */
+         printf("[Generator] [%d]: The Lucas child process returned %d\n", pid_main, ret1);
+         printf("[Generator] [%d]: The Hexagonal child process is %d\n", pid_main, ret2);
+         printf("[Generator] [%d]: The sum of the first n Harmonic numbers is %d\n", pid_main, ret3);
       }
 
       fclose(fp);
